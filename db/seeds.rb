8a91b2677fb851f46e51c17a0ea58062ec2b6057
Carpe-Omnia <40481048+Carpe-Omnia@users.noreply.github.com>
2019-03-27 13:39:34 -0400

    Made site more palatable to potential employers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-tom = User.create(name: "Tomaz", password: "zamot101", email: "tomaz.r.rodrigues@gmail.com", id: 1, inbox_id: 1) 
+tom = User.create(name: "Tomaz", password: "zamot101", email: "tomaz.r.rodrigues@gmail.com", id: 1, inbox_id: 1)
 george = User.create(name: "George", password: "butt", email: "george@gaysex.biz", id: 2, inbox_id: 2)
 box1 = Inbox.create(user_id: tom.id)
 box2 = Inbox.create(user_id: george.id)
 
-everlast = Joke.new 
-everlast.setup = "What do you call a guy who's girlfriend cheats on him forever" 
-everlast.punchline = "Cuck Everlasting" 
-everlast.author = tom 
+everlast = Joke.new
+everlast.setup = "What do you call a guy who's girlfriend cheats on him forever"
+everlast.punchline = "Cuck Everlasting"
+everlast.author = tom
 
-everlast.save 
+everlast.save
 
-gang = Joke.create(setup: "La Cosa Nostra", punchline: "Yeah I'm affiliated with that gang") 
-gang.author = george 
-gang.save 
+gang = Joke.create(setup: "La Cosa Nostra", punchline: "Yeah I'm affiliated with that gang")
+gang.author = george
+gang.save
 
-convo1 = Conversation.new 
-convo1.inboxes << box1 
-convo1.inboxes << box2 
-convo1.save 
+convo1 = Conversation.new
+convo1.inboxes << box1
+convo1.inboxes << box2
+convo1.save
 
-mess1 = Message.new 
-mess1.conversation_id = convo1.id 
-mess1.from_id = george.id 
-mess1.to_id = tom.id 
-mess1.subject = "This is a test message" 
-mess1.content = "This is the content of the test message" 
+mess1 = Message.new
+mess1.conversation_id = convo1.id
+mess1.from_id = george.id
+mess1.to_id = tom.id
+mess1.subject = "This is a test message"
+mess1.content = "This is the content of the test message"
+mess2 = Message.new
+mess2.conversation_id = convo1.id
+mess2.from_id = tom.id
+mess2.to_id = george.id
+mess2.subject = "This is a test message again"
+mess2.content = "This is the content of the test message for a second time"
 
-mess1.save 
+mess1.save
 
-convo1.messages << mess1 
+convo1.messages << mess1
 
-convo1.save 
-
+convo1.save
